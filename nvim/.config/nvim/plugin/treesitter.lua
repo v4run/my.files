82693334ec62054faaf7e20e4c0f2546846d8c46
Varun Vasan V <varunvasan91@gmail.com>
2021-12-26 21:50:23 +0530

    disable treesitter highlight for huge files

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -1,14 +1,20 @@
+local highlight_disabled_langs = { ["vim"] = true }
+
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained", -- "all", "maintained" or list of languages
 	sync_install = false,
 	ignore_install = {},
 	highlight = {
 		enable = true,
-		disable = { "vim" },
+		disable = function(lang, bufnr)
+			return highlight_disabled_langs[lang] or vim.api.nvim_buf_line_count(bufnr) > 100000
+		end,
 		additional_vim_regex_highlighting = false,
 	},
 	refactor = {
 		highlight_definitions = { enable = false },
+		highlight_current_scope = { enable = false },
+		highlight_references = { enable = false },
 	},
 	textobjects = {
 		select = {
