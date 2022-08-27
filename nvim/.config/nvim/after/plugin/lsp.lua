8836bbaefbc44406b01c7162f375d111a20e3b60
Varun Vasan V <varunvasan91@gmail.com>
2022-08-27 21:08:29 +0530

    lua language server fix

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,36 +1,63 @@
-local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
+local keymap = require("varun.keymap")
+local nnoremap = keymap.nnoremap
+local v = vim
 
-	local opts = { noremap = true, silent = true }
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "gc", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "<Leader>p", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("i", "<C-j>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "<Leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	buf_set_keymap("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+local on_attach = function(--[[client, bufnr--]])
+	local opts = { silent = true }
+	nnoremap("gi", function()
+		v.lsp.buf.implementation()
+	end, opts)
+	nnoremap("gc", function()
+		v.lsp.buf.incoming_calls()
+	end, opts)
+	nnoremap("gr", function()
+		v.lsp.buf.references()
+	end, opts)
+	nnoremap("gd", function()
+		v.lsp.buf.definition()
+	end, opts)
+	nnoremap("K", function()
+		v.lsp.buf.hover()
+	end, opts)
+	nnoremap("<Leader>r", function()
+		v.lsp.buf.rename()
+	end, opts)
+	nnoremap("<Leader>p", function()
+		v.lsp.buf.declaration()
+	end, opts)
+	nnoremap("<C-j>", function()
+		v.lsp.buf.signature_help()
+	end, opts)
+	nnoremap("<Leader>D", function()
+		v.lsp.buf.type_definition()
+	end, opts)
+	nnoremap("<Leader>ca", function()
+		v.lsp.buf.code_action()
+	end, opts)
+	nnoremap("<Leader>e", function()
+		v.diagnostic.open_float()
+	end, opts)
+	nnoremap("<Leader>wa", function()
+		v.lsp.buf.add_workspace_folder()
+	end, opts)
+	nnoremap("<Leader>wr", function()
+		v.lsp.buf.remove_workspace_folder()
+	end, opts)
+	nnoremap("<Leader>wl", function()
+		print(v.inspect(v.lsp.buf.list_workspace_folders()))
+	end, opts)
+	nnoremap("[d", function()
+		v.diagnostic.goto_prev()
+	end, opts)
 end
 
 local nvim_lsp = require("lspconfig")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = v.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 -- Merge configs
 local config = function(_config)
-	return vim.tbl_deep_extend("force", {
+	return v.tbl_deep_extend("force", {
 		on_attach = on_attach,
 		capabilities = capabilities,
 		flags = {
@@ -60,6 +87,27 @@ nvim_lsp.pyright.setup(config())
 nvim_lsp.bashls.setup(config())
 nvim_lsp.clangd.setup(config())
 nvim_lsp.tsserver.setup(config())
-nvim_lsp.sumneko_lua.setup(config())
+nvim_lsp.sumneko_lua.setup(config({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}))
 nvim_lsp.yamlls.setup(config())
 nvim_lsp.hls.setup(config())
