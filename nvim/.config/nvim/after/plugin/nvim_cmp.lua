749677f5a2e120bc5e5ab3bae48e1cb7bae477f7
Varun Vasan V <varunvasan91@gmail.com>
2022-08-29 12:45:07 +0530

    refactor

--- nvim/.config/nvim/after/plugin/nvim_cmp.lua
+++ nvim/.config/nvim/after/plugin/nvim_cmp.lua
@@ -69,7 +69,7 @@ if lspkind ~= nil then
 
 			-- The function below will be called before any actual modifications from lspkind
 			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-			before = function(--[[entry--]]_, vim_item)
+			before = function(--[[ entry ]]_, vim_item)
 				return vim_item
 			end,
 		}),
