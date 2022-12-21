0fbf4edc5e7de66dcaa1b5f62c84fa0856a4daa4
Varun Vasan V <varunvasan91@gmail.com>
2022-12-21 19:36:04 +0530

    xinitrc updated + screen layout added

---
+++ x/.screenlayout/current.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 2560x180 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal
