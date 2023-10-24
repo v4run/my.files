5796d98ae4268572349b20de2b8454b64b5369de
Varun Vasan V <varunvasan91@gmail.com>
2023-10-24 20:57:11 +0530

    theme config updated

--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -16,9 +16,11 @@ require("gruvbox").setup({
 	invert_tabline = false,
 	invert_intend_guides = false,
 	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "soft", -- can be "hard", "soft" or empty string
+	contrast = "", -- can be "hard", "soft" or empty string
 	palette_overrides = {},
-	overrides = {},
+	overrides = {
+		SignColumn = { bg = "None" },
+	},
 	dim_inactive = false,
 	transparent_mode = false,
 })
