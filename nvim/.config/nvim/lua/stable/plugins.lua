6fc792abd6a0f9d5639ec7c9bc31977737cbf46c
Varun Vasan V <varunvasan91@gmail.com>
2023-10-27 14:53:18 +0530

    which-key added in nvim

--- nvim/.config/nvim/lua/stable/plugins.lua
+++ nvim/.config/nvim/lua/stable/plugins.lua
@@ -34,6 +34,19 @@ local plugins = {
 	"sbdchd/neoformat",
 	"stevearc/dressing.nvim",
 	"nvim-treesitter/nvim-treesitter-textobjects",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
 local opts = {}
 lazy.setup(plugins, opts)
