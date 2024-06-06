a96e0374aef0db36f492059b46ebcc8a55d32acc
Varun Vasan V <varunvasan91@gmail.com>
2024-06-06 14:34:35 +0530

    script formatted

--- scripts/.scripts/init.sh
+++ scripts/.scripts/init.sh
@@ -6,7 +6,13 @@ __FUNCTION_PREFIX=${__FUNCTION_PREFIX:-}
 # Find the files inside the module directory so that it can be searched using fzf
 # NOTE: Make sure the files are executable
 __find_and_execute_task() {
-	task=$(find "$1" -maxdepth 1 -type f | sort | fzf --query="$2" --info=inline --delimiter='/' --with-nth=-1 --preview-window wrap --preview "bat --color=always --style=numbers --line-range :500 {}")
+	task=$(find "$1" -maxdepth 1 -type f | sort | fzf \
+		--query="$2" \
+		--info=inline \
+		--delimiter='/' \
+		--with-nth=-1 \
+		--preview-window wrap \
+		--preview "bat --color=always --style=numbers --line-range :500 {}")
 	# shellcheck source=/dev/null
 	# The actual script that get executed is located at $__SCRIPT_ROOT_DIR/$1/$task
 	[ -z "$task" ] || . "$task"
@@ -14,12 +20,11 @@ __find_and_execute_task() {
 
 # Load all the currently available modules
 __load_modules() {
-	while IFS= read -r -d '' MODULE_DIR
-	do
+	while IFS= read -r -d '' MODULE_DIR; do
 		MODULE=$(basename "$MODULE_DIR")
 		# Create the function such that it passes the MODULE_DIR and all the parameters used when calling the function to __find_and_execute_task
 		eval "$__FUNCTION_PREFIX$MODULE() { __find_and_execute_task $MODULE_DIR "'"$@"'"; }"
-	done< <(find "$__SCRIPT_ROOT_DIR/" -maxdepth 1 -mindepth 1 -not \( -path "$__SCRIPT_ROOT_DIR/.git" -prune \) -type d -print0)
+	done < <(find "$__SCRIPT_ROOT_DIR/" -maxdepth 1 -mindepth 1 -not \( -path "$__SCRIPT_ROOT_DIR/.git" -prune \) -type d -print0)
 }
 
 # Load modules when script is initialized
