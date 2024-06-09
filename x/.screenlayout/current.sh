2cfee9f6dfd9ac81d3297b7bd8bc2fb7857db29e
Varun Vasan V <varunvasan91@gmail.com>
2024-06-09 20:48:08 +0530

    x config updated

--- x/.screenlayout/current.sh
+++ x/.screenlayout/current.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 2560x180 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 2560x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x180 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
