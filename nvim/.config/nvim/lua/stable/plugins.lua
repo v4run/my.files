894ab78454920d010d6f2678c0500b529eab7a08
Varun Vasan V <varunvasan91@gmail.com>
2024-08-22 13:04:59 +0530

    nvim gen plugin added

--- nvim/.config/nvim/lua/stable/plugins.lua
+++ nvim/.config/nvim/lua/stable/plugins.lua
@@ -25,9 +25,11 @@ local plugins = {
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
 	"hrsh7th/cmp-cmdline",
-	"L3MON4D3/LuaSnip",
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
 	"saadparwaiz1/cmp_luasnip",
-	"rafamadriz/friendly-snippets",
 	"onsails/lspkind.nvim",
 	"nvim-lualine/lualine.nvim",
 	"lewis6991/gitsigns.nvim",
@@ -70,10 +72,8 @@ local plugins = {
 			-- refer to the configuration section below
 		},
 	},
-	{ "nyoom-engineering/oxocarbon.nvim" },
-	{ "loctvl842/monokai-pro.nvim" },
-	{ "nvim-treesitter/playground" },
 	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "David-Kunz/gen.nvim" },
 }
 local opts = {}
 lazy.setup(plugins, opts)
