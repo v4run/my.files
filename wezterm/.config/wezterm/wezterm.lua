0c695249728eade3302d9cfeb87e429510a0bd4b
Varun Vasan V <varunvasan91@gmail.com>
2024-10-20 18:53:20 +0530

    wezterm config update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,14 @@
 local wezterm = require("wezterm")
 local config = {}
-
 config.font = wezterm.font("Iosevka")
 config.color_scheme = "Gruvbox Material (Gogh)"
-config.font_size = 17
+config.font_size = 16
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = false
 config.window_padding = {
-	left = "5px",
+	left = "0px",
 	right = "0px",
-	top = "5px",
+	top = "0px",
 	bottom = "0px",
 }
-
 return config
