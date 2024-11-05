a113e5cd7cd6f8f45fa767672d9b539bc836a051
Varun Vasan V <varunvasan91@gmail.com>
2024-11-05 21:05:59 +0530

    wezterm zenmode config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -17,4 +17,23 @@ config.colors = {
 	cursor_bg = "#ffd866",
 	cursor_border = "#ffd866",
 }
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
 return config
