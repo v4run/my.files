1e49374f129340e7f39a00d97e3c794ed182d082
Varun Vasan V <varunvasan91@gmail.com>
2024-11-07 13:24:33 +0530

    nvim harpoon fixes

--- nvim/.config/nvim/lua/config/stable/autocmds.lua
+++ nvim/.config/nvim/lua/config/stable/autocmds.lua
@@ -1,4 +1,20 @@
 return {
+	harpoon_menu = {
+		opts = {},
+		cmds = {
+			{
+				"VimEnter",
+				opts = {
+					callback = function(--[[ev]])
+						local harpoon = require("harpoon")
+						if harpoon:list():length() > 0 then
+							harpoon.ui:toggle_quick_menu(harpoon:list())
+						end
+					end,
+				},
+			},
+		},
+	},
 	terminal_job = {
 		opts = {},
 		cmds = {
