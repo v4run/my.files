d4822dee64f35c9599c71db4336e1b9e96705b06
Varun Vasan V <varunvasan91@gmail.com>
2024-11-30 12:01:15 +0530

    nvim indent line config update

---
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {
+		indent = { tab_char = "▏", char = "▏" },
+		scope = {
+			show_start = false,
+			enabled = true,
+			show_end = false,
+		},
+	},
+	config = true,
+}
