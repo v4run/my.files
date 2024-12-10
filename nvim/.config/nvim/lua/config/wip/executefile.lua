9dc2bf78fd30b6bb9d05b162feba85a9fdb6caa8
Varun Vasan V <varunvasan91@gmail.com>
2024-12-10 15:20:23 +0530

    test execution binding added

--- nvim/.config/nvim/lua/config/wip/executefile.lua
+++ nvim/.config/nvim/lua/config/wip/executefile.lua
@@ -1,5 +1,5 @@
-local commands = {
-	["rust"] = "!cargo run",
+local exec_commands = {
+	["rust"] = "!cargo run --release",
 	["go"] = "!go run %",
 	["python"] = "!python %",
 	["javascript"] = "!node %",
@@ -8,9 +8,26 @@ local commands = {
 }
 local function exec_file()
 	local filetype = vim.o.filetype
-	if commands[filetype] then
-		vim.cmd(commands[filetype])
+	if exec_commands[filetype] then
+		vim.cmd(exec_commands[filetype])
 	end
 end
 vim.api.nvim_create_user_command("ExecFile", exec_file, {})
 vim.keymap.set("n", "<localleader>e", ":ExecFile<CR>", { desc = "Execute the current file with corresponding program" })
+
+local test_commands = {
+	["rust"] = "!cargo test --release",
+}
+local function test_file()
+	local filetype = vim.o.filetype
+	if test_commands[filetype] then
+		vim.cmd(test_commands[filetype])
+	end
+end
+vim.api.nvim_create_user_command("TestFile", test_file, {})
+vim.keymap.set(
+	"n",
+	"<localleader>t",
+	":TestFile<CR>",
+	{ desc = "Run the test for the current file with corresponding program" }
+)
