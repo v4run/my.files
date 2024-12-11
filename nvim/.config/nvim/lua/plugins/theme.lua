a2e29b6a7caea817fd89698f07b156dd1c7d1d08
Varun Vasan V <varunvasan91@gmail.com>
2024-12-11 23:46:09 +0530

    theme cleanup

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -43,11 +43,6 @@ return {
 		vim.cmd("highlight DiagnosticVirtualTextIconInfo  guifg=#78dce8 guibg=#274246") -- Override hack
 		vim.cmd("highlight DiagnosticVirtualTextIconHint  guifg=#fcfcfa guibg=#4e4c4c") -- Override hack
 
-		-- vim.cmd("highlight DiagnosticVirtualTextError guifg=#ff6188 guibg=#221f22") -- Override hack
-		-- vim.cmd("highlight DiagnosticVirtualTextWarn  guifg=#fc9867 guibg=#221f22") -- Override hack
-		-- vim.cmd("highlight DiagnosticVirtualTextInfo  guifg=#78dce8 guibg=#221f22") -- Override hack
-		-- vim.cmd("highlight DiagnosticVirtualTextHint  guifg=#fcfcfa guibg=#221f22") -- Override hack
-
 		vim.cmd("highlight DiagnosticVirtualTextError guifg=#b4445f guibg=#221f22") -- Override hack
 		vim.cmd("highlight DiagnosticVirtualTextWarn  guifg=#b26a48 guibg=#221f22") -- Override hack
 		vim.cmd("highlight DiagnosticVirtualTextInfo  guifg=#559aa3 guibg=#221f22") -- Override hack
