93860311d917e27578d458058e2579ae08949aa4
Varun Vasan V <varunvasan91@gmail.com>
2025-04-19 15:00:19 +0530

    norg nvim settings updated

--- nvim/.config/nvim/after/ftplugin/norg.lua
+++ nvim/.config/nvim/after/ftplugin/norg.lua
@@ -3,3 +3,5 @@ vim.o.concealcursor = "n"
 vim.opt.listchars = { tab = "  " } -- indent guide lines
 vim.opt.cursorcolumn = false
 vim.opt.signcolumn = "no"
+vim.opt.textwidth = 0
+vim.opt.colorcolumn = ""
