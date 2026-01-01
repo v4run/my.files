82c20fd956a88666abf3df7920e9299178e940a6
Varun Vasan V <varun@MacBookPro.lan>
2026-01-01 09:34:14 +0530

    nvim config update

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -16,7 +16,6 @@ return {
 	},
 	opts = function()
 		local cmp = require("cmp")
-		local lspkind = require("lspkind")
 		local luasnip = require("luasnip")
 		require("luasnip.loaders.from_vscode").lazy_load()
 		local cmp_config = {
@@ -60,22 +59,6 @@ return {
 				{ name = "buffer" },
 				{ name = "neorg" },
 			}, {}),
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text", -- show only symbol annotations
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- can also be a function to dynamically calculate max width such as
-					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-					-- The function below will be called before any actual modifications from lspkind
-					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-					before = function(--[[entry]]_, vim_item)
-						return vim_item
-					end,
-				}),
-			},
 		}
 		return cmp_config
 	end,
